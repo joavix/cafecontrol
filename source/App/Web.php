dcc1cc06ae2dfa4bc72a54e6590250316fea1f4c
joavix <joaovictorpassis@gmail.com>
2023-10-15 17:21:44 -0300

    Criação do controller e autenticação da tela de login

--- source/App/Web.php
+++ source/App/Web.php
@@ -178,10 +178,38 @@ class Web extends Controller
 
     /**
      * SITE LOGIN
+     * @param null|array $data
      * @return void
      */
-    public function login(): void
+    public function login(?array $data): void
     {
+        if (!empty($data['csrf'])) {
+            if (!csrf_verify($data)) {
+                $json['message'] = $this->message->error("Erro ao enviar, favor utilize o formulário")->render();
+                echo json_encode($json);
+                return;
+            }
+
+            if (empty($data['email']) || empty($data['password'])) {
+                $json['message'] = $this->message->warning("Informe seu e-mail e senha para entrar")->render();
+                echo json_encode($json);
+                return;
+            }
+
+            $save = (!empty($data['save']) ? true : false);
+            $auth = new Auth();
+            $login = $auth->login($data['email'], $data['password'], $save);
+
+            if ($login) {
+                $json['redirect'] = url("/app");
+            } else {
+                $json['message'] = $auth->message()->render();
+            }
+
+            echo json_encode($json);
+            return;
+        }
+
         $head = $this->seo->render(
             "Entrar - " . CONF_SITE_NAME,
             CONF_SITE_DESC,
@@ -191,6 +219,7 @@ class Web extends Controller
 
         echo $this->view->render("auth-login", [
             "head" => $head,
+            "cookie" => filter_input(INPUT_COOKIE, "authEmail")
         ]);
     }
 
--- source/Models/Auth.php
+++ source/Models/Auth.php
@@ -3,6 +3,7 @@
 namespace Source\Models;
 
 use Source\Core\Model;
+use Source\Core\Session;
 use Source\Core\View;
 use Source\Support\Email;
 
@@ -42,6 +43,48 @@ class Auth extends Model
             $user->email,
             "{$user->first_name} {$user->last_name}"
         )->send();
+
+        return true;
+    }
+
+    public function login(string $email, string $password, bool $save = false): bool
+    {
+        if (!is_email($email)) {
+            $this->message->warning("O e-mail informado não é válido");
+            return false;
+        }
+
+        if ($save) {
+            setcookie("authEmail", $email, time() + 604800, "/");
+        } else {
+            setcookie("authEmail", null, time() - 3600, "/");
+        }
+
+        if (!is_passwd($password)) {
+            $this->message->warning("A senha informada não é válida");
+            return false;
+        }
+
+        $user = (new User())->findByEmail($email);
+
+        if (!$user) {
+            $this->message->error("O e-mail informado não está cadastrado");
+            return false;
+        }
+
+        if (!passwd_verify($password, $user->password)) {
+            $this->message->error("A senha informada não confere");
+            return false;
+        }
+
+        if (passwd_rehash($user->password)) {
+            $user->password = $password;
+            $user->save();
+        }
+
+        // LOGIN
+        (new Session())->set("authUser", $user->id);
+        $this->message->success("Login efetuado com sucesso")->flash();
         return true;
     }
 }
