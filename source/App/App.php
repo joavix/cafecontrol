8ecfa687f240aba20d454f44b00c024d17eee008
joavix <joaovictorpassis@gmail.com>
2024-03-18 21:49:47 -0300

    Funcionalidade de controle de pagamentos

--- source/App/App.php
+++ source/App/App.php
@@ -340,6 +340,36 @@ class App extends Controller
     }
 
     /**
+     * @param array $data
+     * @return void
+     */
+    public function onpaid(array $data): void
+    {
+        $invoice = (new AppInvoice())
+            ->find("user_id = :user AND id = :id", "user={$this->user->id}&id={$data["invoice"]}")
+            ->fetch();
+
+        if (!$invoice) {
+            $this->message->error("Ooops! Ocorreu um erro ao atualizar o lançamento!")->flash();
+            $json["reload"] = true;
+            echo json_encode($json);
+            return;
+        }
+
+        $invoice->status = ($invoice->status == "paid" ? "unpaid" : "paid");
+        $invoice->save();
+
+        $y = date("Y");
+        $m = date("m");
+        if ($data["date"]) {
+            list($m, $y) = explode("/", $data["date"]);
+        }
+
+        $json["onpaid"] = (new AppInvoice())->balance($this->user, $y, $m, $invoice->type);
+        echo json_encode($json);
+    }
+
+    /**
      * APP INVOICE (Fatura)
      */
     public function invoice()
