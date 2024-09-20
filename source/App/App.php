e439150f5386c4ab69ce7dbc24a01a5a059c7d64
joavix <joaovictorpassis@gmail.com>
2024-09-20 14:54:20 -0300

    funcionalidade para restringir recursos premium

--- source/App/App.php
+++ source/App/App.php
@@ -281,6 +281,16 @@ class App extends Controller
     {
         //create
         if (!empty($data["wallet"]) && !empty($data["wallet_name"])) {
+            //PREMIUM RESOURCE
+            $subscribe = (new AppSubscription())->find("user_id = :user AND status != :status",
+                "user={$this->user->id}&status=canceled");
+
+            if (!$subscribe->count()) {
+                $this->message->error("Desculpe {$this->user->first_name}, para criar novas carteiras é preciso ser PRO. Confira abaixo...")->flash();
+                echo json_encode(["redirect" => url("/app/assinatura")]);
+                return;
+            }
+
             $wallet = new AppWallet();
             $wallet->user_id = $this->user->id;
             $wallet->wallet = filter_var($data["wallet_name"], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
@@ -346,6 +356,25 @@ class App extends Controller
         if (request_limit("applaunch", 20, 60 * 5)) {
             $json["message"] = $this->message->warning("Foi muito rápido {$this->user->first_name}! Por favor aguarde 5 minutos para novos lançamentos.")->render();
             echo json_encode($json);
+            return;
+        }
+
+        $wallet = (new AppWallet())->find("user_id = :user AND id = :id",
+            "user={$this->user->id}&id={$data["wallet"]}")->fetch();
+
+        if (!$wallet) {
+            $json["message"] = $this->message->warning("Ooops, você tentou lançar em uma carteira que não existe ou está indisponível no momento.")->render();
+            echo json_encode($json);
+            return;
+        }
+
+        //PREMIUM RESOURCE
+        $subscribe = (new AppSubscription())->find("user_id = :user AND status != :status",
+            "user={$this->user->id}&status=canceled");
+
+        if (!$wallet->free && !$subscribe->count()) {
+            $this->message->error("Sua carteira {$wallet->wallet} é PRO {$this->user->first_name}. Para controla-la é preciso ser PRO. Assine abaixo...")->flash();
+            echo json_encode(["redirect" => url("/app/assinatura")]);
             return;
         }
 
